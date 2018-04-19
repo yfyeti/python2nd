5d2918d731a6bd091f675a3ef1b12943f54a44ff
yfyeti <yf.yeti@163.com>
2018-04-19 11:55:38 +0800

    羊车门修改

--- p121-4.6.py
+++ p121-4.6.py
@@ -2,13 +2,18 @@ while 1:
     from random import *
     fcho=0
     scho=0
-    a=int(input("请输入2以上的整数表示羊的个数："))
-    time=100000
-    for i in range(time):
-        result=randint(0,a)
-        if result==0:
-            fcho+=1
-        elif randint(0,a-2)==0:
-            scho+=1
+    a=input("请输入2以上的整数表示羊的个数：")
+    if a.isnumeric():
+        a=int(a)
+        time=100000
+        for i in range(time):
+            result=randint(0,a)
+            if result==0:
+                fcho+=1
+            elif randint(0,a-2)==0:
+                scho+=1
+    else:
+        print("整数!!!")
+        continue
     print("不改变选择获得车的概率为：{:.3f}".format(fcho/time))
     print("改变选择获得车的概率为：{:.3f}".format(scho/time))
